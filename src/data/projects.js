3196388af7533b1be8cc1e2be8a07b3e5310d8b1
Anis Riaz <117629024+anisriaz@users.noreply.github.com>
2023-04-10 12:11:23 +0500

    Add files via upload

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+const allMyProjects = [
+    {
+        image: "./Images/Projects/project-1.jpg",
+        title: "POS Function App",
+        description: "POS (Point of Sale) function is a software feature that enables businesses to process transactions, manage inventory, and track sales data at the point of purchase.",
+        githubLink: "https://github.com/anisriaz/POS-Function",
+        demoLink: "https://boisterous-bunny-435d7d.netlify.app",
+    },
+    {
+        image: "./Images/Projects/project-2.jpg",
+        title: "Rock-Paper-Scissor",
+        description: "Rock paper scissors is a hand game played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand, representing either rock paper or scissors.",
+        githubLink: "https://github.com/anisriaz/Rock-Paper-Scissor",
+        demoLink: "https://coruscating-macaron-88f25c.netlify.app",
+    },
+    {
+        image: "./Images/Projects/project-3.jpg",
+        title: "Navbar Design",
+        description: "Beautifull website Navbar Design",
+        githubLink: "https://github.com/anisriaz/Mobile-info",
+        demoLink: "https://stirring-hamster-45d8ce.netlify.app",
+    },
+    {
+        image: "./Images/Projects/project-4.jpg",
+        title: "This is my personal portfolio.",
+        description: "I have created a portfolio website to showcase my skills and work experience.",
+        githubLink: "https://github.com/anisriaz/Portfolio",
+        demoLink: "https://radiant-sunshine-f84005.netlify.app/",
+    },
+
+]
+
+export default allMyProjects;
