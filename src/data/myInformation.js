da8a5fe758cc6d7609acb2fa1df9dfab15dff1f6
Anis Riaz <117629024+anisriaz@users.noreply.github.com>
2024-10-31 15:09:03 +0500

    Update myInformation.js

--- src/data/myInformation.js
+++ src/data/myInformation.js
@@ -18,10 +18,6 @@ const myInformation = {
         aboutMeDescription:
             "I am a Full-Stack developer based in Azad Kashmir, Pakistan, and I am very passionate about improving my coding skills and developing applications and websites using the MERN Stack. I work for myself to enhance my skills, and I love building Full-Stack clones.",
     },
-    contactSection: {
-        web3FormsEndpoint: "https://api.web3forms.com/submit",
-        web3FormsAccessKey: "b5d25ff6-33a1-4193-b520-b94354014d62"
-    },
 };
 
 export default myInformation;
